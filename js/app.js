6472cdeb45a8ded285ed90b0d75ee9a8aee39803
spectermagicc <69127196+spectermagicc@users.noreply.github.com>
2020-08-25 16:01:04 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const secciones = new fullpage('#fullpage', {
+    autoScrolling: true,
+	fitToSection: false,
+	fitToSectionDelay: 300,
+	easing: 'easeInOutCubic',
+	scrollingSpeed: 700,
+	css3: true,
+	easingcss3: 'ease-out',
+    loopBottom: false,
+
+    navigation: true,
+    menu: '#menu',
+	anchors: ['inicio', 'productos', 'contacto'],
+	navigationTooltips: ['INICIO', 'PRODUCTOS', 'CONTACTO'],
+	showActiveTooltip: false,
+
+	sectionsColor : ['#000', '#c2c2c2', '#000'],
+	verticalCentered: true,
+
+	controlArrows: true,
+	slidesNavigation: true,
+
+	afterLoad: function(origin, destination){
+		if(destination.anchor == 'contacto')
+		document.querySelector('.footer').querySelector('h2').style.opacity = 1;
+	
+	}
+});
